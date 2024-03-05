264ee188d9fe5eeed2cf12ca5d00be8d6db6615b
Amanda Vieira <amandasv.pm@gmail.com>
2024-03-05 17:34:30 -0300

    parcialmente add e salvando

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import {useQuery } from 'react-query';
-import axios from 'axios';
+import { useMutation, useQuery, useQueryClient, useQueries } from 'react-query';
+
 
 function App() {
-  const {data, isLoading, } = useQuery("api", () => {
-    return axios
-      .get("http://localhost:5000/api")
-      .then(response => response.data)
-  });
+  const queryClient = useQueryClient();
 
-  if(isLoading) {
-    return <p>Loading</p>
+
+  const coursesQuery = useQuery("courses", () =>
+    axios.get("http://localhost:5000/courses").then((response) => response.data)
+  );
+  const savedQuery =  useQuery("saved", () =>
+  axios.get("http://localhost:5000/saved").then((response) => response.data)
+);
+
+
+  const { data: coursesData, isLoading: isLoadingCourses, error: errorCourses } = coursesQuery
+  const { data: savedData, isLoading: isLoadingSaved, error: errorSaved} = savedQuery
+
+  const mutation = useMutation({
+    mutationFn: (courseId) => {
+      return axios.post('http://localhost:5000/saved', {
+        id: courseId
+      })
+    },
+    onSuccess: (data) => {
+      console.log('course', data);
+    },
+
+  })
+
+  if (coursesQuery.isLoading) {
+    return <div className="loading">Carregando...</div>;
   }
 
-  console.log(data)
+  if (coursesQuery.error) {
+    return <div className="loading">Algo deu errado!</div>;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {data.courses.map((course:any) => (
-          <a>{course.id}</a>
+    <div className="app-container">
+      <div className="todos">
+        <h2>Todos & React Query</h2>
+        {coursesData.map((course: any) => (
+          <div
+            onClick={() =>
+              mutation.mutate(course.id)
+            }
+            key={course.id}
+          >
+            {course.id}
+          </div>
         ))}
-        
-      </header>
+        <hr/>
+        {savedData.map((saved: any) => (
+          <div
+            key={saved.id}
+          >
+            {saved.id}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
