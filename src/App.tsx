219a60ac03682616dec40c970d6a8edb49adde30
Amanda Vieira <amandasv.pm@gmail.com>
2024-03-08 07:46:01 -0300

    add hover color to card

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,95 @@ import Course from './components/course/Course';
 import { Tile } from './components/course/Tile';
 import { useBookmarkData, useSaveBookmark } from './hooks/useBookmarkData';
 import { BookmarkButton } from './components/bookmark/BookmarkButton';
+import { Card, FormControl, Grid, Radio, RadioGroup, Sheet, Typography } from '@mui/joy';
 
 function App() {
 
   const { data: courseListData, isLoading: isLoadingCourse } = useCourseData();
-  const {saveBookmark} = useSaveBookmark();
+  const { saveBookmark } = useSaveBookmark();
   const [selectedCourse, setSelectedCourse] = useState('')
   console.log('selectedCourse', selectedCourse)
-
-
 
   return (
     <div className="app-container">
       <div className="todos">
         <h2>Todos & React Query</h2>
 
+        <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+          <Grid xs={12}>
+            <Grid container justifyContent="center" spacing={4}>
+            <FormControl>
+              <RadioGroup
+                overlay
+                name="member"
+                orientation="horizontal"
+                sx={{ gap: 2 }}
+              >
+                {courseListData?.map((item: Course) => (
+                  <Grid key={item.id}>
+                    <Sheet
+                      component="label"
+                      key={item.id}
+                      variant="outlined"
+                      sx={
+                        [{
+                          p: 2,
+                          display: 'flex',
+                          flexDirection: 'column',
+                          boxShadow: 'sm',
+                          borderRadius: 'md',
+                          borderWidth: 2,
+                          borderSizing: 'border-box'
+                        },
+                        {
+                          '&:hover': {
+                            borderColor: '#4D0001'
+                          }
+                        }
+                      ]
+                        }
+                    >
+                      <Grid
+                        display="flex"
+                        alignItems="flex-end"
+                      > 
+                         <Radio
+                            value={`course${item.id}`}
+                            variant="outlined"
+                            sx={{flexDirection: 'row'}}
+                            color="primary"
+                        />
+                        <Typography level="title-sm" sx={{ marginLeft: '10px' }}>
+                          Virtual Course
+                        </Typography>
+                      </Grid>
 
-        {courseListData?.map((item: Course) => (
-          <>
-          <div onClick={() => setSelectedCourse(item.id)}>
-            <Tile course={item}/> 
-          </div>
-            
-          </>
+                     
+                      <div onClick={() => setSelectedCourse(item.id)}>
+                        <Tile course={item} />
+                      </div>
+                    </Sheet>
+                  </Grid>
+                ))}
+              </RadioGroup>
 
-        ))}
+            </FormControl>
+              
+            </Grid>
+          </Grid>
+        </Grid>
 
         {/* <div
           onClick={() => saveBookmark.mutate('999')}
         >
           clica aqui
         </div> */}
-        <hr/>
-        <hr/>
+        <hr />
+        <hr />
 
-        <BookmarkButton id={selectedCourse}/>
+        <BookmarkButton id={selectedCourse} />
       </div>
-    </div>
+    </div >
   );
 }
 
--- src/components/course/Tile.tsx
+++ src/components/course/Tile.tsx
@@ -1,3 +1,5 @@
+
+import { Card, CardContent } from "@mui/joy";
 import Course from "./Course"
 
 export interface CourseInterface {
@@ -7,24 +9,47 @@ export interface CourseInterface {
 export function Tile({ course }: CourseInterface) {
   const { id, dates, location, instructors } = course
   return (
-    <div>
-      <p>Id: {id} </p>
-      {dates.map((date) => (
-        <>
-          <p>Start: {date[0]}</p>
-          <p>End: {date[1]}</p>
-        </>
-      ))}
-      <p>Location: {location.timezone}</p>
-      {instructors.map((instructor) => (
-        <>
-          <p>
-          First Name: {instructor.first_name}, Last Name: {instructor.last_name}
-          </p>
-        </>
-      ))}
-      <hr/>
-    </div>
+    <>
+    
+      <CardContent>
+        <p>Id: {id} </p>
+        {dates.map((date) => (
+          <>
+            <p>Start: {date[0]}</p>
+            <p>End: {date[1]}</p>
+          </>
+        ))}
+        <p>Location: {location.timezone}</p>
+        {instructors.map((instructor) => (
+          <>
+            <p>
+            First Name: {instructor.first_name}, Last Name: {instructor.last_name}
+            </p>
+          </>
+        ))}
+      
+      </CardContent>
+    
+    
+    {/* <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent> */}
+
+    </>
+    
   )
 }
 
