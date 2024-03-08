145368a05e59de56d9e9ed438df32382cd56eb8b
Amanda Vieira <amandasv.pm@gmail.com>
2024-03-08 19:13:32 -0300

    dia convertido certinho

--- src/components/course/Tile.tsx
+++ src/components/course/Tile.tsx
@@ -2,63 +2,88 @@
 import { Card, CardContent, Typography } from "@mui/joy";
 import Course from "./Course"
 import * as moment from 'moment';
-import { useEffect, useState } from "react";
 
 export interface CourseInterface {
   course: Course;
 }
 
 export interface DateTimeInfo {
-  dayOfWeek: string;
-  month: string;
-  day: string;
+  text: string;
+  time: string;
 }
 
-function formatedDate(timestamp: [number, number][]): string {
+function formatedDate(timestamp: [number, number][]): DateTimeInfo {
 
-  const diasDaSemana: string[] = [];
-    const diasDoMes: string[] = [];
-    const meses: string[] = [];
+  const weekdays: string[] = [];
+  const monthNames: string[] = [];
+  const daysOfMonth: string[] = [];
+  const startTimes: string[] = [];
+  const endTimes: string[] = [];
 
-    for (const pair of timestamp) {
-        const start = moment.unix(pair[0]);
-        const end = moment.unix(pair[1]);
+  for (const pair of timestamp) {
+      const startTime = moment.unix(pair[0]);
+      const endTime = moment.unix(pair[1]);
 
-        const startDay = start.format('dddd');
-        const endDay = end.format('dddd');
-        const month = start.format('MMMM');
-        const startDayOfMonth = start.format('DD');
-        const endDayOfMonth = end.format('DD');
+      const startWeekday = startTime.format('dddd');
+      const endWeekday = endTime.format('dddd');
+      const monthName = startTime.format('MMMM');
+      const startDayOfMonth = startTime.format('DD');
+      const endDayOfMonth = endTime.format('DD');
+      const startTimeFormatted = formatTime(startTime);
+      const endTimeFormatted = formatTime(endTime);
 
-        diasDaSemana.push(startDay, endDay);
-        diasDoMes.push(startDayOfMonth, endDayOfMonth);
-        meses.push(month);
-    }
+      weekdays.push(startWeekday, endWeekday);
+      monthNames.push(monthName);
+      daysOfMonth.push(startDayOfMonth, endDayOfMonth);
+      startTimes.push(startTimeFormatted);
+      endTimes.push(endTimeFormatted);
+  }
 
-    const diasDaSemanaUnicos = diasDaSemana.filter((day, index) => diasDaSemana.indexOf(day) === index);
-    const diasDoMesUnicos = diasDoMes.filter((day, index) => diasDoMes.indexOf(day) === index);
-    const mesesUnicos = meses.filter((month, index) => meses.indexOf(month) === index);
+  const uniqueWeekdays = weekdays.filter((day, index) => weekdays.indexOf(day) === index);
+  const uniqueMonthNames = monthNames.filter((month, index) => monthNames.indexOf(month) === index);
+  const uniqueDaysOfMonth = daysOfMonth.filter((day, index) => daysOfMonth.indexOf(day) === index);
+  const uniqueStartTimes = startTimes.filter((time, index) => startTimes.indexOf(time) === index);
+  const uniqueEndTimes = endTimes.filter((time, index) => endTimes.indexOf(time) === index);
 
-    const diasDaSemanaFormatados = diasDaSemanaUnicos.length > 1 ? `${diasDaSemanaUnicos.join(' & ')}` : `${diasDaSemanaUnicos[0]}`;
-    const diasDoMesFormatados = diasDoMesUnicos.length > 1 ? `${diasDoMesUnicos.join(' & ')}` : `${diasDoMesUnicos[0]}`;
-    const mesFormatado = mesesUnicos.length > 1 ? `${mesesUnicos.join(', ')}` : `${mesesUnicos[0]}`;
+  const formattedWeekdays = uniqueWeekdays.length > 1 ? `${uniqueWeekdays.join(' & ')}` : `${uniqueWeekdays[0]}`;
+  const formattedDaysOfMonth = uniqueDaysOfMonth.length > 1 ? `${uniqueDaysOfMonth.join(' & ')}` : `${uniqueDaysOfMonth[0]}`;
+  const formattedMonthNames = uniqueMonthNames.length > 1 ? `${uniqueMonthNames.join(', ')}` : `${uniqueMonthNames[0]}`;
+  const formattedStartTimes = uniqueStartTimes.join(' - ');
+  const formattedEndTimes = uniqueEndTimes.join(' - ');
 
-    return `${diasDaSemanaFormatados}, ${mesFormatado} ${diasDoMesFormatados}`;
+  const text = `${formattedWeekdays}, ${formattedMonthNames} ${formattedDaysOfMonth}`;
+  const time = `${formattedStartTimes} - ${formattedEndTimes}`;
 
+  return {
+      text: text,
+      time: time
+  };
+
+}
+
+function formatTime(time: moment.Moment): string {
+  const hour = time.format('h');
+  const minute = time.format('mm');
+  const period = time.format('a');
+
+  if (minute === '00') {
+      return `${hour} ${period}`;
+  } else {
+      return `${hour}:${minute} ${period}`;
+  }
 }
 
 export function Tile({course}: CourseInterface) {
-  
-  console.log('course -> ', course.dates)
- 
-
 
   return (
     <>
 
       <CardContent>
         <Typography level="title-lg">
-        {formatedDate(course.dates)}
+        {formatedDate(course.dates).text}
+        <br/>
+        {formatedDate(course.dates).time}
+
         </Typography>
 
       </CardContent>
